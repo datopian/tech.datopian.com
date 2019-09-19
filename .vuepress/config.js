3209b416cedabffb319bfa15f939152df73bc395
Rufus Pollock <rufus@artearthtech.com>
2019-09-19 07:39:26 +0200

    [config][s]: add markdown-it-footnote plus some other markdown config e.g. linkify, typographer etc.

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,6 +1,18 @@
 module.exports = {
   title: 'Datopian Technical Docs',
   description: 'All our technical documentation in one place',
+  markdown: {
+    linkify: true,
+    typographer: true,
+    breaks: true,
+    html: true,
+    toc: {
+      includeLevel: [1, 2, 3]
+    },
+    extendMarkdown: md => {
+      md.use(require('markdown-it-footnote'))
+    }
+  },
   themeConfig: {
     repo: 'https://gitlab.com/datopian/tech/kb',
     repoLabel: 'Contribute!',
